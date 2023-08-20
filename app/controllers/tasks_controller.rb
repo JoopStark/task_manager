e2ae482bb450647272088d4d42fe40f78570834c
Joop Stark <stark.joop@gmail.com>
2023-08-19 23:00:07 -0600

    Complete #show

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,23 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+  end
+
+  def create
+    task = Task.new({
+      title: params[:title],
+      description: params[:description]
+    })
+
+    task.save
+    
+    redirect_to '/tasks'
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+end
